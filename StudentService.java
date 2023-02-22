55f0f9211e56300746251882effe2a87641f86f4
uyyalavasu <uyyalavasu276@gmail.com>
2023-02-22 13:43:11 +0530

    Add by vasu

--- StudentService.java
+++ StudentService.java
@@ -7,5 +7,9 @@ public interface StudentService {
 
 	public String findNameById(Integer id);
 	public List<Integer> findAge(String s_name);
+	int a=10;
+	int b=20;
+	System.out.println("Sum is : "+(a+b));
+	
 
 }
